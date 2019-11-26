19a662803d19fb45faaffdd4dcf053d80c7f2a1e
barron419 <wreall113@gmail.com>
2019-11-26 10:48:31 -0700

    Updated With Corrections

--- week3/src/observable.spec.ts
+++ week3/src/observable.spec.ts
@@ -1,2 +1,9 @@
 import { expect } from 'chai';
 import 'mocha';
+import { source } from './observable';
+
+describe('Test for observable.ts', () => {
+    it("Should output all car manufacturers", () => {
+        source.subscribe(list => expect(list).to.eql(["Ford", "GMC", "Dodge", "Porsche"]))
+    });
+});
--- week3/src/observable.ts
+++ week3/src/observable.ts
@@ -1,7 +1,6 @@
-import { from } from 'rxjs';
-import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 
-//emit array as a sequence of values
-const arraySource = Rx.Observable.from([1,2,3,4,5]);
-//output: 1,2,3,4,5
-const subscribe = arraySource.subscribe(val => console.log(val));
+
+export const source = of(["Ford", "GMC", "Dodge", "Porsche"]);
+
+source.subscribe(cars => console.log(cars));
